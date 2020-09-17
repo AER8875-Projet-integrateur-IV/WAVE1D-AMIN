ef88bbc0ec184bd49eab23394bf18626d6c448b6
rouga <rouga2012@hotmail.fr>
2020-09-17 16:35:35 -0700

    Added results output to a file

--- src/wave1D.cpp
+++ src/wave1D.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
 #include <vector>
 #include "explicitEuler.h"
+#include <fstream>
 
 
 int main() {
 
-    const size_t nx = 61;
-    const double length = 100.0;
-    const double totalTime = 0.25;
-    const double c = 300;
-    const double cfl = 0.9;
+    // Variable declaration
+    size_t nx;
+    double length, totalTime, c, cfl;
 
+    //Prompt the user for input
+    std:: cout << "[length totalTime c cfl nx] (without brackets) : " << std::endl;
+    std::cin >> length >> totalTime >> c >> cfl >> nx;
 
+    // Init file I/O object
+    std::ofstream fileStream("../results/wave1DOutput.dat");
+
+    if (!fileStream)
+    {
+        // Print an error and exit
+        std::cerr << "Error while writing to file" << std::endl;
+        return 1;
+    }
+
+    // writing simulation conditions to file
+    fileStream << "length: " << length << " totalTime: " << totalTime << 
+                " c: " << c << " cfl: " << cfl << " nx: " << nx << "\n\n\n";
+
+    // writing x coordinates to file
+    double dx = length/nx;
+    fileStream << "X coordinates : ";
+    for (size_t i=0; i < nx; i++ ){
+        fileStream << i*dx << " ";
+    }
+    fileStream << "\n\n\n";
+
+    // writing wave equation results to file
     auto u = explicitEuler(length,totalTime,c,cfl,nx);
     for (auto rows : u) {
 
-        for (double values : rows){
-            std::cout << values << " ";
+        for (double values : rows){   
+            fileStream << values << " ";
         }
 
-        std::cout << std::endl;
+        fileStream << "\n";
     }
+
+    fileStream.close();
     
     return 0;
 }
